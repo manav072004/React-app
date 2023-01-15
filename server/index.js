7f88c698a9ce986da2f294ec0670493bf1ecf2f2
manav072004 <manavpatel3674@gmail.com>
2023-01-15 15:22:59 -0800

    new

--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({
+  origin: "https://artivo-app.onrender.com",
+}));
 
 
 
