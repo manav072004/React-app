b4bf24bdd5a070425203e61c06cc56e06568626b
manav072004 <manavpatel3674@gmail.com>
2023-01-14 23:37:42 -0800

    new

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 import path from 'path';
 
-dotenv.config()
 
+
+dotenv.config()
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -22,6 +23,7 @@ app.use('/', express.static("./client/build"))
 
 const CONNECTION_URL = 'mongodb+srv://javascriptmaster:javascriptmaster123@cluster0.abb916h.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
+
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
